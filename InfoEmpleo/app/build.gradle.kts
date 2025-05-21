e4bfbbce73823e7c3f20b21c14360ffad1c25820
reydem <reydem613@gmail.com>
2025-05-21 16:56:04 -0500

    Inicio de infoEmpleo-App
    Conteo: commit 11

--- InfoEmpleo/app/build.gradle.kts
+++ InfoEmpleo/app/build.gradle.kts
@@ -2,7 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.kapt)
-    alias(libs.plugins.kotlin.compose)               // <— Agregamos el plugin Compose aquí
+    alias(libs.plugins.kotlin.compose)               
     alias(libs.plugins.hilt.android.gradle.plugin)
 }
 android {
